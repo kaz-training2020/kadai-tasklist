9b8a64d55fcb1dedf9c9bc0b41a5fc68b853f684
k.k <kaz.training2020@gmail.com>
2020-08-31 07:35:24 +0000

    test

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        // タスク一覧を取得
+        $tasks = Task::all();
+
+        // メッセージ一覧ビューでそれを表示
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    // getでtask/createにアクセスされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        $task = new Task;
+
+        // メッセージ作成ビューを表示
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    // getでtasks/idにアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // タスク詳細ビューでそれを表示
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    // getでtasks/id/editにアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // タスク編集ビューでそれを表示
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
