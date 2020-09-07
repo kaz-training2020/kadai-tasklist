60d4930aff62920c72196399b201e47abf858459
k.k <kaz.training2020@gmail.com>
2020-09-07 08:57:14 +0000

    kadai-auth3

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -29,4 +29,7 @@
     {{-- タスク作成ページへのリンク --}}
     {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 
+    {{-- ログアウトページへのリンク --}}
+    {!! link_to_route('logout.get', 'Logout', [], ['class' => 'btn btn-primary']) !!}
+
 @endsection
